1330d79b2b140dd7d8b0484ccf0afb16177bea37
Chris Turori <chris.turori@gmail.com>
2020-10-24 19:17:58 -0700

    inital commit

---
+++ src/app.py
@@ -0,0 +1,19 @@
+"""
+author: Chris Turori
+"""
+# import packages
+from flask import Flask, request, render_template
+
+# create flask app 
+app = Flask(__name__)
+app.debug = True
+
+# create routes
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    # must set host ip address to make accessible outside of container
+    # run in debug mode for live editting
+    app.run(host='0.0.0.0', debug=True)
