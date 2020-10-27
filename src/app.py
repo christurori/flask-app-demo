5d241d09f8d1f2f101560f5cea53b9ab5ffb6eed
Chris Turori <chris.turori@gmail.com>
2020-10-26 17:08:03 -0700

    added dependencies to requirements file

--- src/app.py
+++ src/app.py
@@ -4,16 +4,17 @@ author: Chris Turori
 # import packages
 from flask import Flask, request, render_template
 
-# create flask app 
+# create flask app
 app = Flask(__name__)
 app.debug = True
 
-# create routes
+# create route
 @app.route("/")
 def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
+    # command line app execution ONLY
     # must set host ip address to make accessible outside of container
     # run in debug mode for live editting
     app.run(host='0.0.0.0', debug=True)
