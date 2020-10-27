d1527dbfdd7812af4f2874723197160668cc2875
Chris Turori <chris.turori@gmail.com>
2020-10-26 17:11:08 -0700

    removed manual app start

--- src/app.py
+++ src/app.py
@@ -12,9 +12,3 @@ app.debug = True
 @app.route("/")
 def home():
     return render_template("index.html")
-
-if __name__ == "__main__":
-    # command line app execution ONLY
-    # must set host ip address to make accessible outside of container
-    # run in debug mode for live editting
-    app.run(host='0.0.0.0', debug=True)
